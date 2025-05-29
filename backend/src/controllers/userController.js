4456fe78f144d798a9413e1522cd21a356fa5ff7
Harsh10 <harshsahani078@gmail.com>
2025-05-29 12:14:16 +0530

    Developed acceptFriendRequest controller

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -71,13 +71,14 @@ export async function sendFriendRequest(req,res) {
             })
         }
 
+        // Check if the user and the receipient are friends already
         if(recipient.friends.includes(userId)){
             return res.status(400).json({
                 message:"You are already friends with this user"
             })
         }
 
-        // check if a req already exists
+        // check if a friend request already exists
         // Understand this
         const existingRequest = await FriendRequest.findOne({
             $or: [
@@ -107,5 +108,47 @@ export async function sendFriendRequest(req,res) {
         return res.status(500).json({
             message:"Internal server error"
         })
+    }
+}
+
+export async function acceptFriendRequest(req,res) {
+
+    try{
+        const { id:requestId } = req.params
+
+        const friendRequest = await FriendRequest.findById(requestId);
+
+        if(!friendRequest){
+            return res.status(404).json({ message:"Friend Request does not exists"})
+        }
+
+        // Verify current user is the receipient
+        if(friendRequest.recipient.toString()!== req.user._id){
+            return res.status(403).json({message:"You are not authorized to accept this request "})
+        }
+
+        friendRequest.status = "accepted"
+
+        await friendRequest.save();
+
+        // add each user to the other's friends array
+        // $addToSet: adds elements to an array only if they do not already exist.
+        await User.findByIdAndUpdate( friendRequest.sender,{
+            $addToSet:{friends:friendRequest.recipient}
+        } )
+
+        await User.findByIdAndUpdate( friendRequest.recipient,{
+            $addToSet:{friends:friendRequest.sender}
+        } )
+
+        res.status(200).json({ 
+            message: "Friend request accepted" 
+        });
+    }
+    catch(e){
+        console.log("Error in acceptFriendRequest controller", error.message);
+        res.status(500).json({ 
+            message: "Internal Server Error" 
+        });
     }
 }
